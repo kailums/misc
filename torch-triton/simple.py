ebc6b358a928d6151f53156834d3a6dd826b3115
kailums <kalu@microsoft.com>
2023-03-27 17:24:54 +0800

    fix

--- torch-triton/simple.py
+++ torch-triton/simple.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 import torch._dynamo
 from torch._functorch.aot_autograd import aot_module_simplified
@@ -8,10 +7,12 @@ class MLP(nn.Module):
   def __init__(self):
     super().__init__()
     self.fc1 = nn.Linear(32, 64)
+    self.ln = nn.LayerNorm(64)
 
   def forward(self, x):
     x = self.fc1(x)
-    x = torch.nn.functional.gelu(x)
+    x = self.ln(x)
+    #x = torch.nn.functional.gelu(x)
     return x
 
 
@@ -34,10 +35,11 @@ if __name__ == '__main__':
                 fw_compiler=my_compiler
         )
 
-    import pdb;pdb.set_trace()
+    #import pdb;pdb.set_trace()
     #compiled_model = torch.compile(backend=my_backend, dynamic=True)(model)
     compiled_model = torch.compile(dynamic=True)(model)
 
     res = compiled_model(input)
 
     print('res shape: ', res.shape, ' dev: ', res.device)
+
